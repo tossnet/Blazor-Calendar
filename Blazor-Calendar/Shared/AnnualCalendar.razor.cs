1e81b103549ccb53e6ec5b9896ff53f978ee971c
Christophe Peugnet <christophe.peugnet@outlook.fr>
2022-03-06 18:27:17 +0100

    .

---
+++ Blazor-Calendar/Shared/AnnualCalendar.razor.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.JSInterop;
+using Blazor_Calendar;
+using Blazor_Calendar.Shared;
+
+namespace Blazor_Calendar.Shared
+{
+    public partial class AnnualCalendar
+    {
+        [Parameter] public int Months { get; set; } = 11;
+
+        private DateTime _firstdate;
+        [Parameter] public DateTime FirstDate
+        {
+            get
+            {
+                if (_firstdate == DateTime.MinValue) _firstdate = DateTime.Today;
+                return _firstdate.Date;
+            }
+            set
+            {
+                _firstdate = value;
+            }
+        }
+        [Parameter] public int HeigthWindow { get; set; } = 500;
+        [Parameter] public int WidthWindow { get; set; } = 800;
+
+        private DateTime m = DateTime.Today;
+        private DateTime d = default;
+    }
+}
