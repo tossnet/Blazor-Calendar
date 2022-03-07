499f50e3350b40227418cb65784dd30dc22280d5
Christophe Peugnet <christophe.peugnet@outlook.fr>
2022-03-07 22:38:19 +0100

    .

---
+++ Blazor-Calendar/Models/Tasks.cs
@@ -0,0 +1,12 @@
+﻿namespace Blazor_Calendar.Models;
+
+public class Tasks
+{
+    public int ID { get; set; }
+    public string Caption { get; set; }
+    public string Code { get; set; }
+    public string Color { get; set; }
+    public string Comment { get; set; }
+    public DateTime DateStart { get; set; }
+    public DateTime DateEnd { get; set; }
+}
--- Blazor-Calendar/Shared/AnnualCalendar.razor.cs
+++ Blazor-Calendar/Shared/AnnualCalendar.razor.cs
@@ -1,18 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Blazor_Calendar.Models;
 using Microsoft.AspNetCore.Components;
-using System.Net.Http;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.Web.Virtualization;
-using Microsoft.JSInterop;
-using Blazor_Calendar;
-using Blazor_Calendar.Shared;
 
 namespace Blazor_Calendar.Shared
 {
@@ -35,6 +22,7 @@ namespace Blazor_Calendar.Shared
         }
         [Parameter] public int HeigthWindow { get; set; } = 500;
         [Parameter] public int WidthWindow { get; set; } = 800;
+        [Parameter] public Tasks[] TasksList { get; set; }
 
         private DateTime m = DateTime.Today;
         private DateTime day = default;
