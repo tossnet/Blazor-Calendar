0cf4ed2db9b4441cc6f3928c311f7fdb3fbb6a02
Christophe Peugnet <christophe.peugnet@outlook.fr>
2022-03-08 18:06:07 +0100

    rename test project

--- Blazor-Calendar/Models/CalendarEvents.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Blazor_Calendar.Models;
-
-public class ClickTaskParameter
-{
-    public List<int> IDList { get; set; }
-    public double X { get; set; }
-    public double Y { get; set; }
-}
