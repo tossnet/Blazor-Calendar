8af55fc3f145c4c2ee2ea8d9fc537ff8d0cf2385
Christophe Peugnet <christophe.peugnet@outlook.fr>
2022-03-17 10:16:41 +0100

    v2.0.1 - some css change and start the monthly view

---
+++ BlazorCalendar/Helpers/Dates.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace BlazorCalendar;
+
+public class Dates
+{
+    public static int GetNumOfDay(int numOfDay)
+    {
+        // The order of the days of the week is different in each country
+        // DayOfWeek : 
+        //   .Sunday    = 0
+        //   .Monday    = 1
+        //   .Tuesday   = 2
+        //   .Wednesday = 3
+        //   .Thursday  = 4
+        //   .Friday    = 5
+        //   .Saturday  = 6
+        //
+        // => if Sunday is the First Day   =>   0  1  2  3  4  5  6
+        // => if Monday is the First Day   =>   1  2  3  4  5  6  0
+        // => if Saturday is the First Day =>   6  0  1  2  3  4  5
+
+        // To test : 
+        //CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("dz-DZ"); // firstDayOfWeek = Saturday
+        //CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("fr-FR"); // firstDayOfWeek = Monday
+        //CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US"); // firstDayOfWeek = Sunday
+
+        if (numOfDay < 0) return 0;
+        if (numOfDay > 6)
+        {
+            // Whatever the value I transform it in the place from 0 to 6
+            numOfDay %= 7;
+        }
+
+        var firstDayOfWeek = CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek;
+
+        if (firstDayOfWeek == DayOfWeek.Saturday)
+        {
+            if (numOfDay == 0) numOfDay = 6;
+            if (numOfDay == 7) numOfDay = 0;
+        }
+        else if (firstDayOfWeek == DayOfWeek.Monday)
+        {
+            if (numOfDay >= 0) numOfDay++;
+            if (numOfDay == 7) numOfDay = 0;
+        }
+
+        return numOfDay;
+    }
+}
---
+++ BlazorCalendar/MonthlyView.razor.cs
@@ -0,0 +1,23 @@
+﻿using BlazorCalendar.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorCalendar;
+
+partial class MonthlyView : CalendarBase
+{
+    private DateTime _firstdate;
+    [CascadingParameter(Name = "FirstDate")]
+    public DateTime FirstDate
+    {
+        get
+        {
+            if (_firstdate == DateTime.MinValue) _firstdate = DateTime.Today;
+            return _firstdate.Date;
+        }
+        set
+        {
+            _firstdate = value;
+        }
+    }
+}
