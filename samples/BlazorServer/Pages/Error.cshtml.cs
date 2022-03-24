d45a5fe994914054481f18a04c37c2290ce6bce9
Christophe Peugnet <christophe.peugnet@outlook.fr>
2022-03-24 09:32:38 +0100

    add Wasm sample

--- samples/BlazorServer/Pages/Error.cshtml.cs
+++ samples/BlazorServer/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace BlazorCalendarTest.Pages
+namespace BlazorServer.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
